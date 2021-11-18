69f7eedb69a160c0fc38713c7bfad4cab4926470
Nordveyl <kkrutoychel.wf@gmail.com>
2021-11-18 23:10:13 +0300

    Update station.rb

--- station.rb
+++ station.rb
@@ -12,19 +12,22 @@ class Station
 
   def count_trains_of_type (type)  
     k = 0
-    @trains_on_station.each { |train| 
-    if train.type == type 
-      k =k +1
-    end   } 
-    k 
+    @trains_on_station.each do |train| 
+      if train.type == type 
+        k += 1
+      end   
+    end
+    k
   end  
 
   def trains_of_type(type)
     trains_type = []
-    @trains_on_station.each { |train| 
-    if train.type == type 
-      trains_type << train 
-    end   } 
+    @trains_on_station.each do |train| 
+      if train.type == type 
+        trains_type << train 
+      end 
+    end 
+    trains_type  
   end  
 
   def delete_train (train) #отправляет поезда 
