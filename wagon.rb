a1456bc6e2fd984c1d1ed578dad0fb4fefffcc55
Nordveyl <kkrutoychel.wf@gmail.com>
2021-11-27 04:25:16 +0300

    Update wagon.rb

--- wagon.rb
+++ wagon.rb
@@ -1,8 +1,15 @@
 class Wagon
-  include Company
+  include Company, Valid
   attr_reader :type 
 
   def initialize(type) #Type: passenger or cargo
     @type = type 
+    validate!
   end 
+
+  protected
+
+  def validate! 
+    raise "Type of wagon can't be empty" if type.empty?
+  end   
 end 
