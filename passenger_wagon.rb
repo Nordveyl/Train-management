9160d34fb21afb016e68d0bb35fdccf385245420
Nordveyl <kkrutoychel.wf@gmail.com>
2021-11-28 22:15:58 +0300

    Update passenger_wagon.rb

--- passenger_wagon.rb
+++ passenger_wagon.rb
@@ -1,7 +1,15 @@
 class PassengerWagon < Wagon 
+  attr_reader :taken_seats, :free_seats
+  def initialize (seats, type = 'passenger')
+    @type = type
+    @free_seats = seats 
+    @taken_seats = 0
+  end 
 
-  def initialize 
-    @type = 'passenger'
-    super
+  def take_seat 
+    if @free_seats != 0
+      @free_seats -= 1
+      @taken_seats +=1
+    end 
   end 
 end 
